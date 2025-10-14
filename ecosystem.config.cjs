ec8e6a14e512708728750caf813993e848b86f21
harshpatel1408 <harshpatel1408@gmail.com>
2025-10-14 17:33:43 +0530

    change in name

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,11 +7,11 @@ module.exports = {
       env: {
         NODE_ENV: "development",
         PORT: 5050,
-        MONGO_URI:"mongodb://127.0.0.1:27017/betanestdb",
+        MONGO_URI:"mongodb+srv://betanestfinance_db_user:r6BPpQZpVIoUqWtd@betanestfinance.jzyhw6m.mongodb.net/betanestdb",
         GMAIL_USER:"betanest.finance@gmail.com",
         GMAIL_PASS:"nvcc ckpc tgxn cgdl",
         JWT_SECRET:"thisismysecretkey",
-        CLIENT_URL:"http://localhost:5050"
+        CLIENT_URL:"betanestfin.com"
       },
       env_production: {
         NODE_ENV: "production",
